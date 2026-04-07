1e9b32b949d15bb1507543062ddf4d23592bbe3f
Machhindra Jagdale <jagdalemachhindra79@gmail.com>
2026-04-07 11:55:21 +0530

    push stramlit app code

---
+++ frontend/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from add_update_ui import add_update_tab
+from amalytics_by_category import analytics_tab
+from analytics_by_months import analytics_months_tab
+
+
+
+st.title("Expense Tracking System")
+
+tab1,tab2 ,tab3= st.tabs(["Add/Update","Analytics", "Analytics By Months",
+])
+with tab1:
+   add_update_tab()
+with tab2:
+   analytics_tab()
+with tab3:
+   analytics_months_tab()
+
+
